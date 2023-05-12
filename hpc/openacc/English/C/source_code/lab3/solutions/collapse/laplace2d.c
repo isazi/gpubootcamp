8dc6d9ba000118354ebee250c45f43256dd19ad4
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-05-12 15:19:35 +0200

    Fix OpenACC bug.

--- hpc/openacc/English/C/source_code/lab3/solutions/collapse/laplace2d.c
+++ hpc/openacc/English/C/source_code/lab3/solutions/collapse/laplace2d.c
@@ -66,8 +66,8 @@ double calcNext(double *restrict A, double *restrict Anew, int m, int n)
 {
     #pragma tuner start calcNext
     double error = 0.0;
-    #pragma acc parallel num_gangs(ngangs) vector_length(nthreads)
-    #pragma acc loop reduction(max:error) copyin(A[:n*m]) copyout(Anew[:n*m]) collapse(2)
+    #pragma acc parallel num_gangs(ngangs) vector_length(nthreads) copyin(A[:n*m]) copyout(Anew[:n*m]) 
+    #pragma acc loop reduction(max:error) collapse(2)
     for( int j = 1; j < n-1; j++)
     {
         for( int i = 1; i < m-1; i++ )
@@ -84,8 +84,8 @@ double calcNext(double *restrict A, double *restrict Anew, int m, int n)
 void swap(double *restrict A, double *restrict Anew, int m, int n)
 {
     #pragma tuner start swap
-    #pragma acc parallel num_gangs(ngangs) vector_length(nthreads)
-    #pragma acc loop copyin(Anew[:n*m]) copyout(A[:n*m]) collapse(2)
+    #pragma acc parallel num_gangs(ngangs) vector_length(nthreads) copyin(Anew[:n*m]) copyout(A[:n*m])
+    #pragma acc loop collapse(2)
     for( int j = 1; j < n-1; j++)
     {
         for( int i = 1; i < m-1; i++ )
